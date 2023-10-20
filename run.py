d748f81340ab5c4c555d4cdf75e200b72973849c
Yasmin Escaros-Buechsel <yescaros@apple.com>
2023-10-20 15:42:57 +0100

    FEAT(run): adding dice roll function

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+from random import randrange
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def roll_dice(dice_nr):
+	'''
+	Function for random number output to simulate dice rolls
+	'''
+	return(randrange(1, dice_nr + 1))
