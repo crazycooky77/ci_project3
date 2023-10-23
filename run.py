a1c6c9ed49559bcb7b8755d08e0c3c75d44cb29a
Yasmin Escaros-Buechsel <yescaros@apple.com>
2023-10-23 12:36:05 +0100

    FEAT(run): adjusting spacing for (terminal) formatting

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from random import randrange
 import time
+import numbers
 
 
 # Your code goes here.
@@ -10,11 +11,10 @@ def roll_dice(roll_request, dice_nr):
 	"""
 	Function for random number output to simulate dice rolls
 	"""
-	input(f'''
-{roll_request}
+	input(f'''{roll_request}
 Enter any text, or just press the "Enter" key, to roll the die.\n''')
 	die_roll = randrange(1, dice_nr + 1)
-	print(f'You rolled {die_roll}.')
+	print(f'You rolled {die_roll}.\n')
 	return die_roll
 
 
@@ -30,12 +30,12 @@ def game_selections(input_req, wrong_input, *choices):
 		# If valid, enter selection into a variable
 		if game_sel.capitalize() in choices:
 			selection = game_sel.capitalize()
-		elif 0 <= int(game_sel) <= len(choices):
+		elif game_sel.isnumeric() and 0 <= int(game_sel) <= len(choices):
 			selection = choices[int(game_sel) - 1]
 
 		# Print the selection to the user if valid
 		try:
-			print(f'You selected {selection}')
+			print(f'\nYou selected {selection}\n')
 			return selection
 		# Repeat the loop if invalid, to get a valid selection
 		except UnboundLocalError:
@@ -47,17 +47,15 @@ def gameplay():
 	"""
 	Function that runs the full game
 	"""
-	print('Welcome to Woodlands and Witches! Let\'s begin our game')
+	print('Welcome to Woodlands and Witches! Let\'s begin our game\n')
 	time.sleep(1)
 
 	# Character selection
-	print('''
-You can choose between 4 characters:
+	print('''You can choose between 4 characters:
 1) A Witch with powerful magical abilities
 2) A Druid who can transform into a jaguar
 3) A mischievous Pixie who attacks with pixie dust
-4) A Nymph, who is a friend to, and will be aided by, trees
-		''')
+4) A Nymph, who is a friend to, and will be aided by, trees\n''')
 	char_sel = 'Type in the number or character type to make your selection: '
 	char_invalid = 'Please type 1 or Witch, 2 or Druid, 3 or Pixie, 4 or Nymph'
 	characters = ['Witch', 'Druid', 'Pixie', 'Nymph']
@@ -65,14 +63,12 @@ You can choose between 4 characters:
 
 	# Pause before continuing the game
 	time.sleep(1)
-	print('''
-You wake up in the middle of a dark forest.
-There are 3 paths ahead of you.
+	print('''You wake up in the middle of a dark forest.
+There are 3 paths ahead of you:
 1) One path leads to a Cave.
 2) One leads to a Clearing.
 3) The last path leads to a River.
-What do you choose?
-		''')
+What do you choose?'\n''')
 
 	# First story choice for the user
 	first_req = 'Type in the number or location to make your selection: '
@@ -88,63 +84,48 @@ What do you choose?
 	time.sleep(1)
 	# Outcome of first die roll
 	if (first_die % 2) == 0:
+		time.sleep(2)
 		# Lucky outcome for even numbers
-		print('''
-A giant panther appears, goes up to you,
+		print('''A giant panther appears, goes up to you,
 and nudges your hand with it's head.
-It wants to make friends!
-		''')
+It wants to make friends!\n''')
 	else:
 		# Unlucky outcome for odd numbers
-		first_odd_roll = ('''
-A giant panther appears, looking at you menacingly...
-What will you do? Roll the dice!
-		''')
+		first_odd_roll = ('''A giant panther appears, looking at you menacingly...
+What will you do? Roll the dice!\n''')
 		# Unlucky outcome die roll
 		first_odd_die = roll_dice(first_odd_roll, 20)
 		if 1 <= first_odd_die <= 3:
 			# Game over for die rolls 1 - 3
-			print('''
-You try attacking the big cat, but you're too slow.
+			print('''You try attacking the big cat, but you're too slow.
 It swipes at you with a giant paw,
-and everything turns black!
-			''')
+and everything turns black!\n''')
 			time.sleep(3)
 			print('You died... Game Over')
 		elif character == 'Witch':
 			# Good Witch outcome for die rolls 4 - 20
-			print('''
-You send a huge blast of arcane magic at the big cat and defeat it.
-It agrees to help you on your quest.
-			''')
+			print('''You send a huge blast of arcane magic at the big cat and defeat it.
+It agrees to help you on your quest.\n''')
 		elif character == 'Pixie':
 			# Good Pixie outcome for die rolls 4 - 20
-			print('''
-You blow a huge cloud of pixie dust into the big cat's face.
-It becomes charmed by you and your dust, and starts following you around.
-			''')
+			print('''You blow a huge cloud of pixie dust into the big cat's face.
+It becomes charmed by you and your dust, and starts following you around.\n''')
 		elif character == 'Nymph':
 			# Good Nymph outcome for die rolls 4 - 20
-			print('''
-You get your tree friends to help you.
+			print('''You get your tree friends to help you.
 They surround the big cat, who jumps up the branches and climbs the tree.
 It falls asleep on the tree limbs, thanking you for the assistance.
-It wants to repay the favour.
-			''')
+It wants to repay the favour.\n''')
 		elif character == 'Druid':
 			# Good Druid outcome for die rolls 4 - 20
-			print('''
-You transform into a jaguar and make friends with the big cat.
-It joins you on your journey!
-			''')
+			print('''You transform into a jaguar and make friends with the big cat.
+It joins you on your journey!\n''')
 
 	# Pause before continuing the game
 	time.sleep(1)
-	print('''
-You continue on and see a little cabin
+	print('''You continue on and see a little cabin
 with smoke coming out of the chimney.
-Do you knock on the door, or barge right in?
-	''')
+Do you Knock on the door, or Barge right in?\n''')
 
 	# Second story choice for the user
 	second_req = 'Type in the number or word to make your selection: '
